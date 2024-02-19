c95b70692f9a3938db99211114acb5cfefb88b36
eekshajain <jaineeksha@gmail.com>
2024-02-20 00:18:58 +0530

    Java Programs

--- DLTE-Java/DLTE-Java-001/src/main/java/basics/service/Banking.java
+++ DLTE-Java/DLTE-Java-001/src/main/java/basics/service/Banking.java
@@ -1,9 +1,56 @@
 package basics.service;
 
+import java.util.Scanner;
+
 public class Banking {
     public static void main(String[] args) {
-        String name="",emailAddress="",address="",IFSCCode="";
+        String name="",emailAddress="",address="",IFSCCode="",password="",confirmPassword="";
         Long mobile=0L,accNumber=0L;
+        int bankType;
+        Scanner scanner=new Scanner(System.in);
         System.out.println("Enter 1 for NetBanking 2 for Mobile Banking");
+        bankType=scanner.nextInt();
+        switch (bankType){
+            case 1:
+                System.out.println("Enter your name");
+                name=scanner.nextLine();
+                System.out.println("Enter address");
+                address=scanner.nextLine();
+                System.out.println("Enter Email address");
+                emailAddress=scanner.next();
+                System.out.println("Enter Account number");
+                accNumber=scanner.nextLong();
+                System.out.println("Enter bank IFSC code");
+                IFSCCode=scanner.next();
+                System.out.println("Enter password");
+                password=scanner.next();
+                System.out.println("Re enter password");
+                confirmPassword=scanner.next();
+                if(password.equals(confirmPassword))
+                System.out.println("Dear customer thank you for registering for Net Banking");
+                else
+                    System.out.println("Wrong password");
+            case 2:
+                System.out.println("Enter your name");
+                name=scanner.nextLine();
+                System.out.println("Enter address");
+                address=scanner.nextLine();
+                System.out.println("Enter Email address");
+                emailAddress=scanner.next();
+                System.out.println("Enter Mobile number");
+                mobile=scanner.nextLong();
+                System.out.println("Enter Account number");
+                accNumber=scanner.nextLong();
+                System.out.println("Enter bank IFSC code");
+                IFSCCode=scanner.next();
+                System.out.println("Enter password");
+                password=scanner.next();
+                System.out.println("Re enter password");
+                confirmPassword=scanner.next();
+                if(password.equals(confirmPassword))
+                    System.out.println("Dear customer thank you for registering for Net Banking");
+                else
+                    System.out.println("Wrong password");
+        }
     }
 }
--- DLTE-Java/DLTE-Java-001/src/main/java/basics/service/PersonalLoan.java
+++ DLTE-Java/DLTE-Java-001/src/main/java/basics/service/PersonalLoan.java
@@ -1,4 +1,30 @@
 package basics.service;
 
+import java.util.Scanner;
+
 public class PersonalLoan {
+    public static void main(String[] args) {
+        String borrowerName="",borrowerPan="",borrowerAddress="",borrowerEmail="",borrowerIncomeType="",borrowerVoterID="";
+        Long mobileNumber=0L,aadhaar=0L;
+        System.out.println("----------Welcome to MyBank-----------");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Fill your Name");
+        borrowerName=scanner.nextLine();
+        System.out.println("Fill your Aadhaar");
+        aadhaar=scanner.nextLong();
+        System.out.println("Fill your Pan details");
+        borrowerPan=scanner.next();
+        System.out.println("Fill your NameLet us know our Income Type");
+        borrowerIncomeType=scanner.next();
+        System.out.println("Enter your Voter ID");
+        borrowerVoterID=scanner.next();
+        System.out.println("Mention Mobile number");
+        mobileNumber=scanner.nextLong();
+        System.out.println();
+        System.out.println("Enter your address");
+        borrowerAddress=scanner.nextLine();
+        System.out.println("Enter your Email ID");
+        borrowerEmail=scanner.next();
+        System.out.println("Dear"+borrowerName+" Thank you for applying for loan!Further details will be sent to "+borrowerEmail+" or will be sent SMS to "+mobileNumber);
+    }
 }
