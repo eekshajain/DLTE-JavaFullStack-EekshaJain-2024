d6f309fbaf4e957d484e548db69483738ca923bd
eekshajain <jaineeksha@gmail.com>
2024-03-05 11:30:26 +0530

    Java Programs

--- DLTE-Java/LiveCoding/EmployeeDetails/src/main/java/org/example/CollectDetails.java
+++ DLTE-Java/LiveCoding/EmployeeDetails/src/main/java/org/example/CollectDetails.java
@@ -17,9 +17,11 @@ public class CollectDetails {
 //        EmployeeAddress[] permanentEmployeeAddress=new EmployeeAddress[size];
 //        EmployeeContactDetails[] employeeContactDetails=new EmployeeContactDetails[size];
         List<EmployeeName> employeeNames=new ArrayList<>();
+        List<EmployeeName> employeeID=new ArrayList<>();
         List<EmployeeAddress> temporaryEmployeeAddress=new ArrayList<>();
        List<EmployeeAddress> permanentEmployeeAddress=new ArrayList<>();
         List<EmployeeContactDetails> employeeContactDetails=new ArrayList<>();
+        List<EmployeeAddress> addresses=new ArrayList<>();
 //        for(int index=0;index<size;index++)
 //        {
 //            try {
@@ -42,7 +44,7 @@ public class CollectDetails {
         do{
           try {
                 System.out.println("Enter your Employee ID");
-               employeeNames.add(employeeDetailCollection.getEmployeeID());
+               employeeID.add(employeeDetailCollection.getEmployeeID());
                 System.out.println("Enter your Full Name:");
                 employeeNames.add(employeeDetailCollection.collectNames());
                 System.out.println("Enter your contact details");
@@ -52,39 +54,40 @@ public class CollectDetails {
                 temporaryEmployeeAddress.add(employeeDetailCollection.collectTemporaryAddress());
                 System.out.println("Enter your Permanent Address:");
                 permanentEmployeeAddress.add(employeeDetailCollection.collectPermanentAddress());
+                  //addresses.add(employeeDetailCollection.collectAddress());
             }catch (InputMismatchException e){
                 System.out.println("Invalid Input");
             }
             System.out.println("Do you want to enter more details");
         }while(scanner.next().equalsIgnoreCase("yes"));
 
-        System.out.println("What do you want to display");
-        while (true) {
-            System.out.println("1.Display Names of Employee\n2.Display names and contact details\n3.Display names and address details\n4.Display all details\n5.Exit");
-            int choice=scanner.nextInt();
-                switch (choice) {
-                case 1:
-                    employeeDetailCollection.displayName(employeeNames);
-                    break;
-                case 2:
-                   employeeDetailCollection.displayName(employeeNames);
-                    employeeDetailCollection.displayContactDetails(employeeContactDetails);
-                    break;
-                case 3:
-                    employeeDetailCollection.displayName(employeeNames);
-                    employeeDetailCollection.displayAddress(temporaryEmployeeAddress, permanentEmployeeAddress);
-                    break;
-                case 4:
-                    employeeDetailCollection.displayName(employeeNames);
-                    employeeDetailCollection.displayContactDetails(employeeContactDetails);
-                    employeeDetailCollection.displayAddress(temporaryEmployeeAddress, permanentEmployeeAddress);
-                    break;
-                default:
-                    scanner.close();
-                    System.exit(0);
-
-            }
-        }
-
+//        System.out.println("What do you want to display");
+//        while (true) {
+//            System.out.println("1.Display Names of Employee\n2.Display names and contact details\n3.Display names and address details\n4.Display all details\n5.Exit");
+//            int choice=scanner.nextInt();
+//                switch (choice) {
+//                case 1:
+//                    employeeDetailCollection.displayName(employeeNames);
+//                    break;
+//                case 2:
+//                   employeeDetailCollection.displayName(employeeNames);
+//                    employeeDetailCollection.displayContactDetails(employeeContactDetails);
+//                    break;
+//                case 3:
+//                    employeeDetailCollection.displayName(employeeNames);
+//                    employeeDetailCollection.displayAddress(temporaryEmployeeAddress, permanentEmployeeAddress);
+//                    break;
+//                case 4:
+//                    employeeDetailCollection.displayName(employeeNames);
+//                    employeeDetailCollection.displayContactDetails(employeeContactDetails);
+//                    employeeDetailCollection.displayAddress(temporaryEmployeeAddress, permanentEmployeeAddress);
+//                    break;
+//                default:
+//                    scanner.close();
+//                    System.exit(0);
+//
+//            }
+//        }
+       employeeDetailCollection.displayAll(employeeID,employeeNames,employeeContactDetails,temporaryEmployeeAddress,permanentEmployeeAddress);
     }
 }
--- DLTE-Java/LiveCoding/EmployeeDetails/src/main/java/org/example/EnterDetails.java
+++ DLTE-Java/LiveCoding/EmployeeDetails/src/main/java/org/example/EnterDetails.java
@@ -17,22 +17,38 @@ public class EnterDetails {
        return employeeID;
    }
    public EmployeeName collectNames(){
+       EmployeeName name=new EmployeeName();
        String nameExpression="^[a-zA-Z .']+$";
        System.out.println("Enter your First Name");
-       firstName=scanner.next();
+       //firstName=scanner.next();
+       name.setFirstName(scanner.next());
 //       Pattern patternFirstName=Pattern.compile(nameExpression);
 //       Matcher matcherFName=patternFirstName.matcher(firstName);
        System.out.println("Enter your middle name");
-       middleName=scanner.next();
+      // middleName=scanner.next();
+       name.setLastName(scanner.next());
 //       Pattern patternMiddleName=Pattern.compile(nameExpression);
 //       Matcher matcherMName=patternMiddleName.matcher(middleName);
        System.out.println("Enter your last name");
-       lastName=scanner.next();
+         name.setLastName(scanner.next());
 //       Pattern patternLastName=Pattern.compile(nameExpression);
 //       Matcher matcher=patternLastName.matcher(lastName);
-     return new EmployeeName(firstName,middleName,lastName);
+     //return new EmployeeName(firstName,middleName,lastName);
+   return name;
    }
+ public EmployeeName collectBasic(){
+     EmployeeName name=new EmployeeName();
+     System.out.println("Enter your Employee ID");
+     name.setEmployeeID(scanner3.nextInt());
+     System.out.println("Enter your First Name");
+     name.setFirstName(scanner.next());
+     System.out.println("Enter your middle name");
+     name.setLastName(scanner.next());
+     System.out.println("Enter your last name");
+     name.setLastName(scanner.next());
+     return name;
 
+ }
    public EmployeeContactDetails collectContactDetails(){
      EmployeeContactDetails  employeeContactDetails=new EmployeeContactDetails();
         System.out.println("Enter your phone number");
@@ -59,6 +75,32 @@ public class EnterDetails {
 
    public EmployeeAddress collectPermanentAddress(){
       EmployeeAddress employeeAddress =new EmployeeAddress();
+       System.out.println("Enter your House name");
+       employeeAddress.setPermanentHouseName(scanner2.nextLine());
+       System.out.println("Enter your Street name");
+       employeeAddress.setPermanentHouseStreet(scanner2.nextLine());
+       System.out.println("Enter your City name");
+       employeeAddress.setPermanentCityName(scanner2.nextLine());
+       System.out.println("Enter your State name");
+       employeeAddress.setPermanentStateName(scanner2.nextLine());
+       System.out.println("Enter your city pincode");
+       employeeAddress.setPermanentPinCode(scanner2.nextInt());
+       return employeeAddress;
+   }
+   public EmployeeAddress collectAddress(){
+       EmployeeAddress employeeAddress=new EmployeeAddress();
+       System.out.println("Enter Temporary Address");
+       System.out.println("Enter your House name");
+       employeeAddress.setTemporaryHouseName(scanner1.nextLine());
+       System.out.println("Enter your Street name");
+       employeeAddress.setTemporaryHouseStreet(scanner1.nextLine());
+       System.out.println("Enter your City name");
+       employeeAddress.setTemporaryCityName(scanner1.nextLine());
+       System.out.println("Enter your State name");
+       employeeAddress.setTemporaryStateName(scanner1.nextLine());
+       System.out.println("Enter your city pincode");
+       employeeAddress.setTemporaryPinCode(scanner1.nextInt());
+       System.out.println("Enter Permanent Address");
        System.out.println("Enter your House name");
        employeeAddress.setPermanentHouseName(scanner2.nextLine());
        System.out.println("Enter your Street name");
@@ -99,6 +141,24 @@ public class EnterDetails {
             System.out.println("Permanent address:"+permAddress.getPermanentHouseName()+","+permAddress.getPermanentHouseStreet()+","+permAddress.getPermanentCityName()+","+permAddress.getPermanentStateName()+"-"+permAddress.getPermanentPinCode());
         }
 
+    }
+    public void displayAll(List<EmployeeName> employeeID,List<EmployeeName> employeeName,List<EmployeeContactDetails> employeeContactDetails,List<EmployeeAddress> tempEmployeeAddress, List<EmployeeAddress> permEmployeeAddress){
+        for(int index=0;index<employeeName.size();index++){
+            EmployeeName id=employeeID.get(index);
+            EmployeeName employee=employeeName.get(index);
+            EmployeeContactDetails employeeContactDetails1=employeeContactDetails.get(index);
+            EmployeeAddress tempAddress=tempEmployeeAddress.get(index);
+            EmployeeAddress permAddress=permEmployeeAddress.get(index);
+            System.out.println("Employee ID:"+id.getEmployeeID());
+            if(employee.getFirstName()!=null) {
+                if(employee.getMiddleName()!=null)
+                    if(employee.getLastName()!=null)
+                        System.out.println("Name:" + employee.getFirstName() + " " + employee.getMiddleName() + " " + employee.getLastName());
+            }
+            System.out.println("Phone Number:"+employeeContactDetails1.getPhoneNumber()+"\n"+"Email Address:"+employeeContactDetails1.getEmailID());
+            System.out.println("Temporary address:"+tempAddress.getTemporaryHouseName()+","+tempAddress.getTemporaryHouseStreet()+","+tempAddress.getTemporaryCityName()+","+tempAddress.getTemporaryStateName()+"-"+tempAddress.getTemporaryPinCode());
+            System.out.println("Permanent address:"+permAddress.getPermanentHouseName()+","+permAddress.getPermanentHouseStreet()+","+permAddress.getPermanentCityName()+","+permAddress.getPermanentStateName()+"-"+permAddress.getPermanentPinCode());
+        }
     }
 
 
