0773c0cdf5b22cd80847f3030fb1f9cde4a6df50
eekshajain <jaineeksha@gmail.com>
2024-04-16 21:57:59 +0530

    Project

--- DLTE-Project/DatabaseRepository/PaymentWebservices/src/main/java/com/payment/webservices/exceptions/PayeeException.java
+++
@@ -1,7 +0,0 @@
-package com.payment.webservices.exceptions;
-
-public class PayeeException extends  RuntimeException {
-    public PayeeException(String message) {
-        super(message);
-    }
-}
